d2d124d63d52cbefdeb36a67f68c61ffddc2d784
UserXAni <dandiaj210900@gmail.com>
2024-12-19 18:18:51 +0700

    Template for AideCN

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import com.example.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+  
+}
